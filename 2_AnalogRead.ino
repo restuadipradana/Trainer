a9bc7d4b27a93ee827f3796e2e46366b9be37662
Restu Adi Pradana <restu.maduki@live.com>
2019-08-13 16:18:14 +0700

    Update 2_AnalogRead.ino

--- 2_AnalogRead.ino
+++ 2_AnalogRead.ino
@@ -2,13 +2,13 @@
 
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(pot, INPUT);
+  Serial.begin(9600);  // inisialisasi baud rate serial monitor
+  pinMode(pot, INPUT); //set pin 33 sebagai input potensiometer
 }
 
 void loop()
 {
-  int value = analogRead(pot);
-  Serial.println(value);
+  int value = analogRead(pot); //membaca nilai analog
+  Serial.println(value);       //mencetak nilai analog input ke serial monitor
   delay(300);
 }

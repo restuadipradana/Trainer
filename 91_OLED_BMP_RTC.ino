4880897eaa3ee7a0bb744b52dc67af1204fb576f
Restu Adi Pradana <restu.maduki@live.com>
2019-08-28 16:17:15 +0700

    Create 91_OLED_BMP_RTC.ino

---
+++ 91_OLED_BMP_RTC.ino
@@ -0,0 +1,91 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_BMP085.h>
+#include "RTClib.h"
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for SSD1306 display connected using software SPI (default case):
+#define OLED_MOSI   23
+#define OLED_CLK   18
+#define OLED_DC    4
+#define OLED_CS    5
+#define OLED_RESET 15
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
+  OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+Adafruit_BMP085 bmp;
+RTC_DS3231 rtc;
+
+char daysOfTheWeek[7][12] = {"Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"};
+
+void setup() {
+  Serial.begin(9600);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  if (!bmp.begin()) {
+  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+  while (1) {}
+  }
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+ 
+}
+
+void loop() {
+  
+  DateTime now = rtc.now();
+
+  display.clearDisplay();
+
+  // Display Text
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print(now.hour(), DEC);
+  display.print(':');
+  display.print(now.minute(), DEC);
+  display.print(':');
+  display.println(now.second(), DEC);
+  display.println(daysOfTheWeek[now.dayOfTheWeek()]);
+  display.print(now.day(), DEC);
+  display.print('/');
+  display.print(now.month());
+  display.print('/');
+  display.print(now.year(), DEC);
+  display.println();
+  display.print("Suhu: ");
+  display.print(bmp.readTemperature());
+  display.println(" C");
+  display.print("Tekanan: ");
+  display.print(bmp.readPressure());
+  display.println(" Pa");
+  int rp = bmp.readPressure();
+  display.print("Tinggi: ");
+  display.print(bmp.readAltitude());
+  display.println(" m");
+  display.print("Tinggi 2: ");
+  display.print(bmp.readAltitude(rp));
+  display.println(" m");
+  display.display();
+  delay(500);
+  display.clearDisplay();
+}
